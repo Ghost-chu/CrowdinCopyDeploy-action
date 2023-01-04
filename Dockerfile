74a3a5403d8039903f18f50564a78a5231744438
Ghost_chu <ghostchu@111.com>
2023-01-04 17:31:12 +0800

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM eclipse-temurin:17-jdk
+
+LABEL "com.github.actions.name"="CrowdinCopyDeploy"
+LABEL "com.github.actions.description"="Create a Crowdin's Over-The-Air distribution environment on local server"
+LABEL "com.github.actions.icon"="refresh-cw"
+LABEL "com.github.actions.color"="gray-dark"
+
+LABEL version="1.0.0"
+LABEL repository="https://github.com/Ghost-chu/CrowdinCopyDeploy"
+LABEL homepage="https://github.com/Ghost-chu/CrowdinCopyDeploy"
+LABEL maintainer="Ghost_chu <ghostchu@111.com>"
+
+ADD entrypoint.sh /entrypoint.sh
+
+ENTRYPOINT ["/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+set -e
+
+if [ -z "$CROWDIN_ACCESS_TOKEN" ]; then
+  echo "CROWDIN_ACCESS_TOKEN is not set. Quitting."
+  exit 1
+fi
+
+if [ -z "$CROWDIN_PROJECT_BRANCH_ID" ]; then
+  echo "CROWDIN_PROJECT_BRANCH_ID is not set. Quitting."
+  exit 1
+fi
+
+if [ -z "$CROWDIN_PROJECT_ID" ]; then
+  echo "CROWDIN_PROJECT_ID is not set. Quitting."
+  exit 1
+fi
+
+# Default to us-east-1 if AWS_REGION not set.
+if [ -z "$DEPLOY_PATH" ]; then
+  DEPLOY_PATH="./deploy-target"
+fi
+
+export CROWDIN_ACCESS_TOKEN=$CROWDIN_ACCESS_TOKEN
+export CROWDIN_PROJECT_BRANCH_ID=$CROWDIN_PROJECT_BRANCH_ID
+export CROWDIN_PROJECT_ID=$CROWDIN_PROJECT_ID
+export DEPLOY_PATH=$DEPLOY_PATH
+
+java -Xmx2G -jar crowdincopydeploy-1.0-SNAPSHOT.jar
